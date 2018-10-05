c876c6404b6a800180b261224727e7f71d016d8c
Trayce <UTwolfy6@gmail.com>
2018-10-05 09:41:13 -0600

    chapter 1 added

---
+++ TrayceMExercise1-5.java
@@ -0,0 +1,13 @@
+//Trayce Martineau 9-19-18
+class TrayceMartineau1_5 {
+	public static void main(String[] args) {
+		//double or whatever
+		double total = 0;
+		
+		//maths
+		total = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+		
+		//print
+		System.out.print("(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) equals: " + total);
+	}
+}
