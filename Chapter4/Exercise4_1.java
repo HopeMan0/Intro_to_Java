b8f0b15b2743751a8fa882f3d22d07c5b184cb85
Trayce <traycemartineau@yahoo.com>
2018-10-24 11:06:43 -0600

    Added chpt4

---
+++ Chapter4/Exercise4_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Exercise4_1 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		System.out.print("Enter length from vertex to center: ");
+		double length = input.nextDouble();
+		
+	}
+}
