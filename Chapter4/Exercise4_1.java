d0b4c4ac62ccffad8ac859c3298aa9272c199b6e
Trayce <student@HSiMac4-9.local>
2018-10-26 10:04:54 -0600

    added an assignment

--- Chapter4/Exercise4_1.java
+++ Chapter4/Exercise4_1.java
@@ -1,3 +1,4 @@
+//Trayce martineau 10-26-18
 import java.util.*;
 
 class Exercise4_1 {
@@ -5,7 +6,15 @@ class Exercise4_1 {
 		
 		Scanner input = new Scanner (System.in);
 		System.out.print("Enter length from vertex to center: ");
-		double length = input.nextDouble();
+		double radius = input.nextDouble();
 		
+		double area = 0;
+		double side = 0;
+		
+		
+		side = (2 * radius) * Math.sin(Math.PI / 5);
+		area = (5 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 5));
+
+		System.out.println("The area of the pentaggon: " + area);
 	}
 }
---
+++ Chapter4/Exercise4_17.java
@@ -0,0 +1,40 @@
+//Taryce Martineau 10-26-18
+
+import java.util.*;
+
+class Exercise4_17 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		System.out.print("Enter year: ");
+		int year 		= input.nextInt();
+		System.out.print("Enter month (first 3 letters): ");
+		String month 	= input.next();
+		
+		
+		boolean isLeapYear =
+		(year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+			
+		if(month = Jan || Mar || Jul || Aug || Oct || Dec){
+			int days = 31;
+			System.out.println("Days: " + days);
+		}
+		else if( month = Apr || Jun || Sep || Nov){
+			int days = 30;
+			System.out.println("Days: " + days);
+		}
+		else if( month = Feb){
+			if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+			int days = 29;
+			System.out.println("Days: " + days);
+			}
+			else{
+				int days = 28;
+				System.out.println("Days: " + days);
+			}
+		}
+		else{
+			System.out.println("Type a real month, silly");
+		}
+	}
+}
