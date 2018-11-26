ec3d4cf7e673e4d7b57e91d57af4f29778897f0f
Kevin <kevin@gmail.com>
2018-11-26 10:04:05 -0700

    update assignment

--- Chapter 5/Exercise5_17.java
+++ Chapter 5/Exercise5_17.java
@@ -4,6 +4,18 @@ class Exercise5_17 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int number = 0;
+		int user = 0;
+		
+		System.out.print("Enter the number of rows (1-15): ");
+		user = input.nextInt();
+		
+		while (user > 15 || user < 1){
+			System.out.print("Enter the number of rows (1-15): ");
+			user = input.nextInt();
+		}
+		
+		for (int countDracula = 1; countDracula <= user; count++){
+			
+		}
 	}
 }
