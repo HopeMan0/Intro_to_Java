480b5f3b96a227c03ac3c8b3d338fd21c82318ef
Trayce <60021283@ironschools.org>
2018-11-29 07:44:48 -0700

    new assignments

---
+++ Chapter 10/Circle2D.java
@@ -0,0 +1,84 @@
+//Trayce Martineau
+//11-29-18
+import java.util.*;
+
+class Circle2D {
+	//list attributes
+	double x;
+	double y;
+	double radius;
+	
+	//no-args constructor
+	public Circle2D(){
+		this.x		 = 0;
+		this.y		 = 0;
+		this.radius  = 1;
+	}
+	
+	//ALL-args constructor
+	public Circle2D(double x, double y, double radius){
+		this.x 		= x;
+		this.y 		= y;
+		this.radius = radius;
+	}
+	//mutators
+	public double getX(){
+		return this.x;
+	}
+	public double getY(){
+		return this.y;
+	}
+	public double getRadius(){
+		return this.radius;
+	}
+	//area
+	public double getArea(){
+		return Math.PI * Math.PI * this.radius;
+	}
+	//cirucmference
+	public double getCircumference(){
+		return Math.PI * (2 * radius);
+	}
+	//return true if the point is in the circle
+	public boolean contains(double x, double y){
+		if (x < (this.x this.radius) && x > (this.x - this.radius) &&
+			y < (this.y + this.radius) && y > (this.y - this.radius)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	//returns true if the circle is in the cricle
+	public boolean contains(Circe2D circle) {
+		//solve to find left, right, top, and bottom points
+		double leftX 	= circle.getX()  - circle.getRadius();
+		double rightX	= circle.getX() + circle.getRadius();
+		double topY 	= circle.getY()	 + circle.getRadius();
+		double bottomY 	= circle.getY()	 - circle.getRadius();
+
+		//if points are in the circle, return true
+		if (this.contains(leftX, circle.getY()) && this.contains(rightX, circle.getY()) &&
+			this.contains(circle.getX(), topY) && this.contains(circle.getX(), bottomY)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	//returns true if circle overlaps this circle, else false
+	public boolean overlaps (Circle2D circle){
+		double distance = Math.sqrt(Math.pow(this.getX() - circle.getX(), 2) + 
+									Math.pow(this.getY() - circle.getY(), 2));
+									
+		//if the distance between centers is less than or equal to sum or radii,
+		//then the circles overlap
+		if (distance <= (this.getRadius() + circle.getRadius())) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
---
+++ Chapter 10/Exercise10_11.java
@@ -0,0 +1,9 @@
+//Trayce Martineau
+//11-29-18
+import java.util.*;
+
+class Exercise10_11 {
+	public static void main(String[] args) {
+		
+	}
+}
