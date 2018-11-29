b9fde10af4fdaf02164f5419db29cade9f8fbdf8
Trayce <60021283@ironschools.org>
2018-11-29 06:58:56 -0700

    edit stuff

--- Chapter 10/Account.java
+++ Chapter 10/Account.java
@@ -12,73 +12,73 @@ public class Account {
 	private Date dateCreated;
 
 
-public Account() {
-	id = 0; 
-	balance = 100;
-	annualInterestRate = 0;
-	dateCreated = new Date();
+	public Account() {
+		id = 0; 
+		balance = 0;
+		annualInterestRate = 0;
+		dateCreated = new Date();
 	}	
 
-public Account(int newid, double newBalance) {
-	id = newid;
-	balance = newBalance;
-	dateCreated = new Date();
+	public Account(int newid, double newBalance) {
+		id = newid;
+		balance = newBalance;
+		dateCreated = new Date();
 	}
 
-public void setid(int newid){
-	id = newid;
+	public void setid(int newid){
+		id = newid;
 	}
 
 	// Set balance 
-public void setBalance(double newBalance) {
+	public void setBalance(double newBalance) {
 		balance = newBalance;
 	}
 
 	// Set annualInterestRate
-public void setAnnualInterestRate(double newAnnualInterestRate) {
+	public void setAnnualInterestRate(double newAnnualInterestRate) {
 		annualInterestRate = newAnnualInterestRate;
 	}
 
 
 
 	//Return id
-public int getId() {
+	public int getId() {
 		return id;
 	}
 
 	// Return balance
-public double getBalance() {
+	public double getBalance() {
 		return balance;
 	}
 
 	// Return annualInterestRate
-public double getAnnualInterestRate() {
+	public double getAnnualInterestRate() {
 		return annualInterestRate;
 	}
 
 	// Return dateCreated
-public String getDateCreated() {
+	public String getDateCreated() {
 		return dateCreated.toString();
 	}
 
 	// Return monthly interest rate
-public double getMonthlyInterestRate() {
+	public double getMonthlyInterestRate() {
 		return annualInterestRate / 12;
 	}
 
 	// Methods
 	// Return monthly interest
-public double getMonthlyInterest() {
+	public double getMonthlyInterest() {
 		return balance * (getMonthlyInterestRate() / 100);
 	}
 
 	// Decrease balance by amount
-public void withdraw(double amount) {
+	public void withdraw(double amount) {
 		balance = balance - amount;
 	}
 
 	// Increase balance by amount
-public void deposit(double amount) {
+	public void deposit(double amount) {
 		balance = balance + amount;
 	}
 }
--- Chapter 10/Exercise10_7.java
+++ Chapter 10/Exercise10_7.java
@@ -6,41 +6,54 @@ import java.util.*;
 class Exercise10_7 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		System.out.print("ID: ");
-		int pin = input.nextInt();
 
 		int choice;
-		double withdraw;
-		double deposit;
 		
+		Account[] atm = new Account[10];
 		
-		while(pin != -1){
-			if(pin == 0 || pin == 1 || pin == 2 || pin == 3 || pin == 4 || pin == 5 || pin == 6 || pin == 7 || pin == 8 || pin == 9){
+		for(int i = 0; i < 10; i++){
+			atm[i] = new Account(i, 100);
+		}
+		
+		System.out.print("ID (-1 to quit): ");
+		int id = input.nextInt();
+		
+		while(id != -1){
+			if(id == 0 || id == 1 || id == 2 || id == 3 || id == 4 || id == 5 || id == 6 || id == 7 || id == 8 || id == 9){
 				System.out.println("Main menu");
 				System.out.println("1: check balance");
 				System.out.println("2: withdraw");
 				System.out.println("3: deposit");
 				System.out.println("4: exit");
 				choice = input.nextInt();
+				double balance = atm[id].getBalance();
 				//switch for menu choices
 				switch(choice){
-					case 1 : System.out.println(Account.getBalance);
-					break;
-					case 2 : System.out.print("Enter amount you'd like to withdraw: ");
-						withdraw = input.nextDouble();
-						Account.setBalance = Account.getBalance - withdraw;
-					break;
-					case 3 : System.out.print("Enter amount you'd like to deposit: ");
-						deposit = input.nextDouble();
-						Account.setBalance = Account.getBalance + deposit;
-					default: System.out.println("Enter Valid choice!!");
-					case 4 : pin = -1;
+					case 1 : System.out.println(balance);
+						break;
+					case 2 : 
+						System.out.print("Enter amount you'd like to withdraw: ");
+						double withdraw = input.nextDouble();
+						balance = balance - withdraw;
+						atm[id].setBalance(balance);
+						break;
+					case 3 : 
+					System.out.print("Enter amount you'd like to deposit: ");
+						double deposit = input.nextDouble();
+						balance = balance + deposit;
+						atm[id].setBalance(balance);
+						break;
+					case 4 : id = -1;
+					default: 
+						System.out.println("Enter Valid choice!!");
 				}
+				System.out.println("Balance: " + balance);
 			}
 			else{
-				System.out.println("Enter a valid pin");
+				System.out.println("Enter a valid id");
 			}
+			System.out.print("ID (-1 to quit): ");
+			id = input.nextInt();
 		}
 	}
 }
