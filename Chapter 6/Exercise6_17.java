d857714e0c158e700e76c583dbdca0b070b26db3
404kevnotfound <student@HSiMac4-9.local>
2018-12-10 10:04:06 -0700

    help

--- Chapter 6/Exercise6_17.java
+++ Chapter 6/Exercise6_17.java
@@ -6,13 +6,17 @@ class Exercise6_17 {
 		
 		Scanner input   = new Scanner(System.in);
 		int n 	 		= input.nextInt();
+		printMatrix(n);
 	}
 	public static void printMatrix(int n){
-		for(int i = 0; i < n; i++){
-			int rando = (int)(0 + Math.random() * 1 + 1);
-			System.out.print(" " + rando);
+		
+		for(int e = 0; e < n; e++){
+			System.out.println("");
+			for(int i = 0; i < n; i++){
+				int rando = (int)(0 + Math.random() * 1 + 5);
+				System.out.print(" " + rando);
 			//System.out.println((int(0 + Math.random())) * 1 + 1)
-			
+			}
 		}
 	}
 }
